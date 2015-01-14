9f56241fadfa0fec350d057c060b2c3d64517103
Felix Bartels <felix@host-consultants.de>
2015-01-14 19:22:15 +0100

    remove testing output, reduce time to two months

--- z-push-delete-old-states.sh
+++ z-push-delete-old-states.sh
@@ -2,17 +2,13 @@
 IFS=$'\n'
 ZPA=/usr/share/z-push/z-push-admin.php
 
-#date +"%Y-%m-%d"
-SIXMONTHSAGO=$(date +"%s" --date='6 months ago')
-#echo $SIXMONTHSAGO
+TWOMONTHSAGO=$(date +"%s" --date='2 months ago')
 
 for i in $($ZPA -a lastsync | tail -n +6); do
         DEVICE=$(echo $i | awk '{print $1}')
         USER=$(echo $i | awk '{print $2}')
         LASTDATE=$(echo $i | awk '{print $3}')
-        #LASTDATE2=$(date -d $LASTDATE +%s)
 
-        #echo $LASTDATE2
         if [[ $LASTDATE == *"never"* ]]; then
                 echo "$DEVICE never synced"
                 $ZPA -a remove -u $USER -d $DEVICE
@@ -20,8 +16,8 @@ for i in $($ZPA -a lastsync | tail -n +6); do
         else
                 LASTDATE2=$(date -d $LASTDATE +%s)
         fi
-        if [[ $SIXMONTHSAGO -ge $LASTDATE2 ]]; then
-                echo "$DEVICE is older than 6 months"
+        if [[ $TWOMONTHSAGO -ge $LASTDATE2 ]]; then
+                echo "$DEVICE is older than 2 months"
                 $ZPA -a remove -u $USER -d $DEVICE
         fi
 done
